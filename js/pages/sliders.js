238f047a53f08127a6737d76e438f94f73a92e65
Seregin Mikle <mikhail-seregin@yandex.ru>
2019-03-26 17:53:37 +0300

    adaptive index.html 50%

--- js/pages/sliders.js
+++ js/pages/sliders.js
@@ -11,6 +11,24 @@ $(function () {
             el: '.ispecs__pag',
             clickable: true
         },
+        breakpoints: {
+            // when window width is <= 320px
+            320: {
+                // slidesPerView: 1,
+                // spaceBetween: 10
+            },
+            // when window width is <= 480px
+            480: {
+                // slidesPerView: 2,
+                // spaceBetween: 20
+            },
+            // when window width is <= 640px
+            1199.99: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 20
+            }
+        }
 
     });
 
--- smartgrid.js
+++ smartgrid.js
@@ -12,19 +12,23 @@ smartgrid('./css/layout', {
     breakPoints: {
         lg: {
             width: "1199.99px",
-            fields: "10px"
+            fields: "10px",
+            containerWidth: '768px'
         },
         md: {
             width: "991.99px",
-            fields: "10px"
+            fields: "10px",
+            containerWidth: '768px'
         },
         sm: {
             width: "767.99px",
-            fields: "10px"
+            fields: "10px",
+            containerWidth: '320px'
         },
         xs: {
             width: "575.99px",
-            fields: "10px"
+            fields: "10px",
+            containerWidth: '320px'
         }
     },
 });
